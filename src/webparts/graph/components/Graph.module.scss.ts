b3247aa257cf6a487ac55840ed771f5140440d8f
Adrien Veillault <adrien.veillault@michelin.com>
2018-08-07 10:27:24 +0200

    Add hover effect

--- src/webparts/graph/components/Graph.module.scss.ts
+++ src/webparts/graph/components/Graph.module.scss.ts
@@ -1,11 +1,11 @@
 /* tslint:disable */
 require('./Graph.module.css');
 const styles = {
-  graph: 'graph_67533382',
-  box: 'box_67533382',
-  close: 'close_67533382',
-  bottomContent: 'bottomContent_67533382',
-  row: 'row_67533382',
+  graph: 'graph_b598c1b9',
+  box: 'box_b598c1b9',
+  close: 'close_b598c1b9',
+  bottomContent: 'bottomContent_b598c1b9',
+  row: 'row_b598c1b9',
 };
 
 export default styles;
